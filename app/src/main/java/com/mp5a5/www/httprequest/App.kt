8e57a03d852cfe9ecf9f6a208ea4c0a5203751f2
Dollon <wwb199055@126.com>
2019-01-09 16:43:22 +0800

    优化代码

--- app/src/main/java/com/mp5a5/www/httprequest/App.kt
+++ app/src/main/java/com/mp5a5/www/httprequest/App.kt
@@ -35,7 +35,7 @@ class App : Application() {
         val build = ApiConfig.Builder()
             .setBaseUrl(baseUrl)//BaseUrl，这个地方加入后项目中默认使用该url
             .setInvalidateToken(0)//Token失效码
-            .setSucceedCode(200)//成功返回码
+            .setSucceedCode(0)//成功返回码
             /*
              *    Token失效后发送动态广播，配合BaseObserver中的标识进行接收使用
              *    public static final String TOKEN_INVALID_TAG = "token_invalid";
