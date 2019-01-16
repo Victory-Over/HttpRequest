7c29c8c6ce31a71c16577af709e1813dd10ba323
Dollon <wwb199055@126.com>
2019-01-16 16:43:37 +0800

    优化代码

--- app/src/main/java/com/mp5a5/www/httprequest/net/api/NbaService.java
+++ app/src/main/java/com/mp5a5/www/httprequest/net/api/NbaService.java
@@ -6,12 +6,11 @@ import com.mp5a5.www.library.use.RetrofitFactory;
 import io.reactivex.Observable;
 
 /**
- * @author ：mp5a5 on 2019/1/16 10：09
+ * @author ：mp5a5 on 2019/1/16 16：37
  * @describe
  * @email：wwb199055@126.com
  */
 public class NbaService {
-
 
     private NBAApiT nbaApiT;
 
@@ -20,18 +19,17 @@ public class NbaService {
     }
 
     public static NbaService getInstance() {
-        return NbaServiceHolder.S_INSTANCE;
+        return Nbaservice1Holder.S_INSTANCE;
     }
 
-    private static class NbaServiceHolder {
+    private static class Nbaservice1Holder {
         private static final NbaService S_INSTANCE = new NbaService();
     }
 
-
     public Observable<NBAEntity> getNBAInfo(String key) {
-        ArrayMap<String, Object> arrayMap = new ArrayMap<String, Object>();
-        arrayMap.put("key", key);
-        return nbaApiT.getNBAInfo(arrayMap);
+        ArrayMap<String, Object> map = new ArrayMap<>();
+        map.put("key", key);
+        return nbaApiT.getNBAInfo(map);
     }
 
 }
